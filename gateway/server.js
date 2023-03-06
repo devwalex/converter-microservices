b393bb5e3b57c94c0fab774e45cbc0d353ab0e8a
devwalex <salamiusmanolawale@gmail.com>
2023-03-06 14:35:57 +0100

    feat: upload video to gridfs and publish to rabbitmq

--- gateway/server.js
+++ gateway/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const { register, login, validate } = require("./services/auth");
+const multer = require("multer");
+const mongoose = require("mongoose");
+const { GridFsStorage } = require("multer-gridfs-storage");
+const amqplib = require("amqplib");
 require("dotenv/config");
 const app = express();
 const router = express.Router();
+const { register, login, validate } = require("./services/auth");
+
+let bucket;
+const connection = mongoose.createConnection("mongodb://127.0.0.1:27017/videos");
+connection.once("open", () => {
+  bucket = new mongoose.mongo.GridFSBucket(connection);
+});
+const videoStorage = new GridFsStorage({ db: connection });
+const upload = multer({ storage: videoStorage }).single("video");
 
 // parse json request body
 app.use(express.json());
@@ -37,6 +49,40 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/upload", upload, async (req, res) => {
+  try {
+    const response = await validate(req);
+    if (response.data) {
+      const videoFileId = req.file.id;
+      const message = {
+        videoFileId: videoFileId.toString(),
+        audioFileId: null,
+        userEmail: response.data.email,
+      };
+      console.log("message", message);
+
+      const stringifyMessage = JSON.stringify(message);
+
+      const queue = "videos";
+      const conn = await amqplib.connect("amqp://localhost");
+      const channel = await conn.createChannel();
+      await channel.assertQueue(queue, { durable: true });
+      channel.sendToQueue(queue, Buffer.from(stringifyMessage), { persistent: true });
+
+      console.log(" [x] Sent '%s'", stringifyMessage);
+      await channel.close();
+
+      return res.status(201).send("Video uploaded successfully!");
+    }
+  } catch (error) {
+    console.log("Upload Error =>", error);
+    if (error.response) {
+      return res.status(500).send(error.response.data);
+    } else {
+      return res.status(500).send(error);
+    }
+  }
+});
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Gateway service is running on port ${PORT}`));
