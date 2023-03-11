8a06168a85ef753c4a66d2b9b4bde138381f5dcb
devwalex <salamiusmanolawale@gmail.com>
2023-03-11 22:41:48 +0100

    chore: add db host for auth service

--- auth/knexfile.js
+++ auth/knexfile.js
@@ -7,6 +7,7 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
